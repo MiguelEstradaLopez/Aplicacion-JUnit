54226d70eeb0fff9284b1559f7cf95820cf36029
Miki <maestralopez766@gmail.com>
2025-11-14 02:46:46 -0500

    refactor: reorganizar importaciones en CalculatorControllerTest para mejorar la legibilidad

--- src/test/java/com/example/junitdemo/web/CalculatorControllerTest.java
+++ src/test/java/com/example/junitdemo/web/CalculatorControllerTest.java
@@ -1,16 +1,16 @@
 package com.example.junitdemo.web;
 
-import com.example.junitdemo.service.CalculatorService;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.junitdemo.service.CalculatorService;
 
 @WebMvcTest(controllers = CalculatorController.class)
 class CalculatorControllerTest {
